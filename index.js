5cbdba84bf787abfe32efa73a7a168be6d668013
RajavarapuNageswarao <129726602+RajavarapuNageswarao@users.noreply.github.com>
2024-06-07 12:09:31 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//objects
+//is the real world entities so like key and value pairs
+
+//simple object declaration and initializtion
+const obj = { name: 'john', age: 23, prop: true };
+console.log(obj);
+
+//expanded object
+
+const objAnother = {
+  name: 'doe',
+  age: 23,
+  isAdmin: true,
+  hobbies: ['crick', 'code', 'sharing'],
+  action:function(){
+    greet(){
+      console.log("This is ")
+    }
+  }
+};
+
+console.log(objAnother.age);
